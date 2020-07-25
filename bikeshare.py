de3098a8ab5494bcdec232d46be68c5eaf3407e7
sambendje <jsambendje@gmail.com>
2020-07-25 16:05:41 +0200

    refactor: changed display 10 lines of data instead of 5

--- bikeshare.py
+++ bikeshare.py
@@ -232,7 +232,7 @@ def main():
         print("Would you like to see 5 more lines of data ? Enter yes or no ")
         display_data = input()
         display_data = display_data.lower()
-        i = 5
+        i = 10
         while display_data != 'yes':
             break
             """ Display 5 lines of raw data for the user"""
